07ae8ff1276a826ac176fe99ea90f5b43426a095
A. Unique TensorFlower <gardener@tensorflow.org>
2026-04-18 21:42:24 -0700

    Automated Code Change
    Reverts c926ad1c96d55c6d2b967db6df64893729232f87
    
    PiperOrigin-RevId: 901996657

--- tensorflow/core/platform/env_test.cc
+++ tensorflow/core/platform/env_test.cc
@@ -305,14 +305,15 @@ class TmpDirFileSystem : public NullFileSystem {
                           TransactionToken* token) override {
     absl::string_view scheme, host, path;
     io::ParseURI(dir, &scheme, &host, &path);
-    if (path.empty()) return errors::NotFound(dir, " not found");
+    if (path.empty())
+      return absl::NotFoundError(absl::StrCat(dir, " not found"));
     // The special "flushed" file exists only if the filesystem's caches have
     // been flushed.
     if (path == "/flushed") {
       if (flushed_) {
         return absl::OkStatus();
       } else {
-        return errors::NotFound("FlushCaches() not called yet");
+        return absl::NotFoundError("FlushCaches() not called yet");
       }
     }
     return Env::Default()->FileExists(io::JoinPath(BaseDir(), path));
@@ -323,10 +324,10 @@ class TmpDirFileSystem : public NullFileSystem {
     absl::string_view scheme, host, path;
     io::ParseURI(dir, &scheme, &host, &path);
     if (scheme != "tmpdirfs") {
-      return errors::FailedPrecondition("scheme must be tmpdirfs");
+      return absl::FailedPreconditionError("scheme must be tmpdirfs");
     }
     if (host != "testhost") {
-      return errors::FailedPrecondition("host must be testhost");
+      return absl::FailedPreconditionError("host must be testhost");
     }
     absl::Status status =
         Env::Default()->CreateDir(io::JoinPath(BaseDir(), path));
@@ -343,7 +344,7 @@ class TmpDirFileSystem : public NullFileSystem {
     io::ParseURI(dir, &scheme, &host, &path);
     for (const auto& existing_dir : created_directories_)
       if (existing_dir == path) return absl::OkStatus();
-    return errors::NotFound(dir, " not found");
+    return absl::NotFoundError(absl::StrCat(dir, " not found"));
   }
 
   void FlushCaches(TransactionToken* token) override { flushed_ = true; }
